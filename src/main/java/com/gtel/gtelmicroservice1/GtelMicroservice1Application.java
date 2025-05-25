39e2ef3c1babc37da0d2c06ec19db6ec45be853f
sontk <sontk@sphinxjsc.com>
2025-05-25 17:27:54 +0700

    init-1

---
+++ src/main/java/com/gtel/gtelmicroservice1/GtelMicroservice1Application.java
@@ -0,0 +1,13 @@
+package com.gtel.gtelmicroservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtelMicroservice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtelMicroservice1Application.class, args);
+    }
+
+}
