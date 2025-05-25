b52ef80a4916939d1fe9366c19ccd3eb242b2056
sontk <sontk@sphinxjsc.com>
2025-05-25 17:31:34 +0700

    init

---
+++ order/src/main/java/com/gtel/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.gtel.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
---
+++ warehouse/src/main/java/com/gtel/warehouse/WarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.gtel.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseApplication.class, args);
+    }
+
+}
